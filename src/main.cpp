a1f8a4f8765801f7129adf8947effa75010a8227
MaGa <guser1996.gu@gmail.com>
2015-11-23 19:40:56 +0100

    implemented test training perceptron for logic or

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,48 @@
 #include "Perceptron.h"
 #include "Neuron.h"
 #include "Layer.h"
+#include "StepFunction.h"
+#include "SigmoidFunction.h"
 
+Perceptron logicOrTest ()
+{
+    double threshold = 0;
+    Perceptron orNN (StepFunction (threshold), 2, vector <int> ({2, 1}));
+
+    vector <pair <vector <double>, vector <double>> samples;
+    samples.push_back (make_pair ({0, 0}, {0}));
+    samples.push_back (make_pair ({1, 0}, {1}));
+    samples.push_back (make_pair ({0, 1}, {1}));
+    samples.push_back (make_pair ({1, 1}, {1}));
+
+    auto iTrainSample = samples.begin();
+    while (iTrainSample != samples.end())
+    {
+        double error = 0;
+        bool immediateMatch = true;
+        do
+        {
+            error = orNN.Train (iTrainSample->first, iTrainSample->second);
+            if (error != 0)
+                immediateMatch = false;
+        }while (error != 0);
+
+        if (!immediateMatch)
+            samples.insert (samples.end(), samples.begin(), iTrainSample);
+        else
+            ++iTrainSample;
+    }
+}
+
+Perceptron logicAndTest ()
+{
+
+}
+
+Perceptron linearFunctionTest ()
+{
+
+}
 
 int main()
 {
